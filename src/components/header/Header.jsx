2181efd63a0c2dcda407ef81592ad294d0b37ea2
Mirko <giancarlo1234522@gmail.com>
2024-04-22 23:39:16 -0500

    update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,12 @@
 import { UserAuth } from '../../context/AuthContextProvider';
 import { useNavigate } from 'react-router-dom';
+import { DivMirko } from './sHeader';
+import { Wrapper2 } from '../../pages/theme/generalStyles/generalStyles';
+
 const Header = () => {
   const { logOut } = UserAuth();
   const navigate = useNavigate();
+
   const handleLogOuth = async () => {
     try {
       await logOut();
@@ -13,8 +17,12 @@ const Header = () => {
   };
   return (
     <>
-      <div>Header</div>
-      <button onClick={handleLogOuth}>salir</button>
+      <Wrapper2>
+        <DivMirko>
+          <div>Header</div>
+          <button onClick={handleLogOuth}>salir</button>
+        </DivMirko>
+      </Wrapper2>
     </>
   );
 };
